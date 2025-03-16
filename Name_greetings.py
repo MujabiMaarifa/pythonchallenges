ae998e3bba6719d89aecae3c2c484423d11ddc2b
DAVID MUJABI <daudimujabi@gmail.com>
2025-03-16 19:50:19 +0300

    Add files via upload

---
+++ Name_greetings.py
@@ -0,0 +1,8 @@
+"""1. Personalized Greeting App 👋
+Create a program that asks for the user’s name and favorite color, then prints a personalized greeting like: "
+"“Hello, [Name]! Your favorite color, [Color], is awesome!” """
+
+name = input("Enter your name: ")
+color = input("What is your favourite color : ")
+print("Hello " + name + ", You just inserted your favourite color as: " + color + " is awesome!!")
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pythonchallenges
+# python_challenges
+This repo contains simple python challenges creating pathway to problem solving
